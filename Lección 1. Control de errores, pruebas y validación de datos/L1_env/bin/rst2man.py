6a64223f9dfbffb7c230d25b41aec553bfdaeb1e
Descoler <obertol@gmail.com>
2021-07-10 09:55:27 -0700

    L1

---
+++ Lección 1. Control de errores, pruebas y validación de datos/L1_env/bin/rst2man.py
@@ -0,0 +1,26 @@
+#!/home/obert/opt/Buenas practicas de programacion en Python/Lección 1. Control de errores, pruebas y validación de datos/L1_env/bin/python
+
+# Author:
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  " + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
